d1e5ebea34b57f4454399d65c1052d80a21e917f
MrnMartin <martindesousamarinabella@gmail.com>
2025-04-11 12:12:48 +0200

    Nationalidad

---
+++ scripts/Nationalite.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+
+file_path = 'results-survey293392-4.4.xlsx'
+df= pd.read_excel(file_path)
+print (df.columns)
+
+col_nationalite = 'Votre nationalité?'
+
+européens = [
+    "Français","Française","Italienne","Italien","Portugais","Portugaise","Espagnol","Espagnole","Belge","Allemand","Allemande","Suisse","Néerlandais","Néerlandaise","Suédois","Suédoise", "Danois","Danoise","Finlandais", "Finlandaise", "Irlandais","Irlandaise", "Norvégien",'Norvégienne',"Polonais", "Polonaise", "Grec", "Grecque", "Roumain","Roumaine","Tchèque", "Slovaque","Hongrois","Hongroise","Croate","Serbe","Letton","Lituanien","Estonien","Slovène","Chypriote","Maltais", "Autrichien", "Autrichienne"
+    ]
+
+def europ(nationalite):
+    if pd.isna(nationalite):
+        return "Non-européen"
+    nationalite = nationalite.lower()
+    for europe in européens:
+        if europe.lower() in nationalite:
+            return "Européen"
+        return "Non-européen"
+
+df[col_nationalite] = df[col_nationalite].apply(europ)
+
+df.to_excel('Resultats_nat.xlsx', index=False)
+
+print(df[[col_nationalite]].head())
